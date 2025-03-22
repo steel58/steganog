d597923fa731cbaa209039d499a1faa77e5d1768
Harper Nordin <harper.nordin@gmail.com>
2025-03-21 23:29:24 -0700

    updated documentation

--- img_ops.h
+++ img_ops.h
@@ -5,8 +5,7 @@
 
 
 /* Embeds the given "msg" in the last bits of the "r" "g" and "b" values respecively
- * "msg" does not have any length requirement but a length of 14 is assumed when retrieving
- * a "msg".
+ * "msg" does not have any length requirement 
  * REQUIREMENT: Image must have minimum len(msg) * 8 pixels
  *
  */
@@ -14,8 +13,7 @@ void hide_message(Image *image, const char *msg);
 
 
 /* Retrieves a message that has been embeded by "hide_message".
- * Expects message to be of length 14
+ * Has now length requirement
  * Requires that image meets same size standards of hide_message
- * msg is the buffer that the message will be written into and data there will not be saved
  */
 char* decode_message(Image *image);
